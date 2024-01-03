90b6fa3387fbcb4229ceade899c7d369471e2413
wind <zurish@qq.com>
2024-01-03 11:06:19 +0800

    feature: 完成接口类

--- services/my-rpc/rpc-core/src/main/java/cn/zurish/snow/core/rpc/transport/RpcClient.java
+++ services/my-rpc/rpc-core/src/main/java/cn/zurish/snow/core/rpc/transport/RpcClient.java
@@ -1,8 +1,15 @@
 package cn.zurish.snow.core.rpc.transport;
 
+import cn.zurish.snow.core.rpc.serializer.CommonSerializer;
+import cn.zurish.snow.rpc.common.entity.RpcRequest;
+
 /**
  * 客户端类通用接口
  * 2024/1/1 23:26
  */
 public interface RpcClient {
+
+    int DEFAULT_SERIALIZER = CommonSerializer.KRYO_SERIALIZER;
+
+    Object sendRequest(RpcRequest request);
 }
--- services/my-rpc/rpc-core/src/main/java/cn/zurish/snow/core/rpc/transport/RpcServer.java
+++ services/my-rpc/rpc-core/src/main/java/cn/zurish/snow/core/rpc/transport/RpcServer.java
@@ -1,8 +1,16 @@
 package cn.zurish.snow.core.rpc.transport;
 
+import cn.zurish.snow.core.rpc.serializer.CommonSerializer;
+
 /**
  * 服务器类通用接口
  * 2024/1/1 23:26
  */
 public interface RpcServer {
+
+    int DEFAULT_SERIALIZER = CommonSerializer.KRYO_SERIALIZER;
+
+    void start();
+
+    <T> void publishService(T service, String serviceName);
 }
